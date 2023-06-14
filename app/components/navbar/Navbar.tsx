5849f3597a81d51ae16b5f070593246b347044ec
AndresEspinoz4 <eandresfelipe8@gmail.com>
2023-06-14 01:01:45 -0500

    version 1

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+export default function Navbar() {
+    return (
+        <nav className="bg-zinc-900 text-white text-center">
+            <div className="flex justify-between"> 
+                <div> 
+                    <img className="w-7" src="https://th.bing.com/th/id/R.2d4fd478e760a8230ce32488662b6629?rik=LAiuUASDHWXbvw&pid=ImgRaw&r=0"></img>
+                </div>
+                <div> <h2 className="text-xl">Busca</h2></div>
+                <div> <h2 className="text-xl">Busca</h2></div>
+            </div>
+
+           
+        </nav>
+    )
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+import Navbar from './components/navbar/Navbar';
+
+export default function Home() {
+  return (
+    <div>
+    <Navbar></Navbar>
+    <h1 className='text-cente text-blue-700 mt-32'>perras</h1>
+    </div>
+  )
+}
